79de638784c7e892d3a07a7fb650d55891c18f6b
ByeongDeokLee <bdlee6710@gmail>
2025-05-27 00:04:41 +0900

    25/05/27 -1

--- src/ViewFame/FrameFolder/SubMainHome.jsx
+++ src/ViewFame/FrameFolder/SubMainHome.jsx
@@ -5,11 +5,39 @@ import dog1 from "../../assets/img/dog1.jpg";
 import dog2 from "../../assets/img/dog2.jpg";
 import dog3 from "../../assets/img/dog3.jpg";
 import dog4 from "../../assets/img/dog4.jpg";
-import dog5 from "../../assets/img/dog5.jpg";
 
 //라이브러리
 
-const HomePageImg = [dog1, dog2, dog3, dog4, dog5];
+const HomePageImg = [
+  {
+    src: dog1,
+    title: "기업 제작용 페이지",
+    great: 4.1,
+    price: 50000,
+    producer: "핑크덕후",
+  },
+  {
+    src: dog2,
+    title: "개쩌는 홈페이지",
+    great: 3.9,
+    price: 500,
+    producer: "길동이",
+  },
+  {
+    src: dog3,
+    title: "요즘 많이 사용하는 홈페이지",
+    great: 3.0,
+    price: 5000,
+    producer: "오리쿵덕",
+  },
+  {
+    src: dog4,
+    title: "간편한 홈페이지 제작",
+    great: 4.5,
+    price: 25000,
+    producer: "피캬츄",
+  },
+];
 
 export default function SubMainHome() {
   return (
@@ -18,13 +46,15 @@ export default function SubMainHome() {
         <h1>홈페이지을 원하시면 여기 보세요.</h1>
         {HomePageImg.map((img, index) => {
           return (
-            <div className="home_page_image" key={index}>
-              <img
-                src={img}
-                alt={`슬라이드 ${index + 1}`}
-                // class="home_page_image"
-                // style={{ width: "100%", borderRadius: "12px" }}
-              />
+            <div className="home_page_card" key={index}>
+              <img src={img.src} alt={img.title} className="home_page_image" />
+              <span className="home_page_title">{img.title}</span>
+              <div className="home_page_great">{img.great.toFixed(1)}</div>
+              <div className="home_page_price">
+                {img.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+                  "원 ~"}
+              </div>
+              <div className="home_page_producer">{img.producer}</div>
             </div>
           );
         })}
