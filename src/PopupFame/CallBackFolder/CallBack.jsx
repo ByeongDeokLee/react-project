bb1dedf5c7c5e356dddb03f04f69f47ff2f4093f
ByeongDeokLee <bdlee6710@gmail>
2025-06-02 01:11:44 +0900

    25/06/02-2

--- src/PopupFame/CallBackFolder/CallBack.jsx
+++ src/PopupFame/CallBackFolder/CallBack.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import axios from "axios";
 
 //카카오
 const KAKAO_REST_API_KEY = "c2c0630ecffa6b96e8c78ef64478cf57";
@@ -28,76 +29,85 @@ export default function KakaoCallback() {
     const state = url.searchParams.get("state") || "";
     const pathname = url.pathname;
 
-    console.log("\n\n\n\n URL \n\n\n", url);
-
     if (pathname.includes("kakao") || state === "" || state === "kakao") {
-      // Step 1: access token 발급
-      fetch("https://kauth.kakao.com/oauth/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-        body: new URLSearchParams({
-          grant_type: "authorization_code",
-          client_id: KAKAO_REST_API_KEY,
-          redirect_uri: KAKAO_REDIRECT_URI,
-          code: code,
-        }),
-      })
-        .then((res) => res.json())
-        .then((tokenData) => {
-          const accessToken = tokenData.access_token;
-
+      axios
+        .post(
+          "https://kauth.kakao.com/oauth/token",
+          new URLSearchParams({
+            grant_type: "authorization_code",
+            client_id: KAKAO_REST_API_KEY,
+            redirect_uri: KAKAO_REDIRECT_URI,
+            code: code,
+          }),
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+            },
+          }
+        )
+        .then((res) => {
+          const accessToken = res.data.access_token;
+          localStorage.setItem("kakaoAccessToken", accessToken);
           // Step 2: 사용자 정보 요청
-          return fetch("https://kapi.kakao.com/v2/user/me", {
-            method: "GET",
+          return axios.get("https://kapi.kakao.com/v2/user/me", {
             headers: {
               Authorization: `Bearer ${accessToken}`,
               "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
             },
           });
         })
-        .then((res) => res.json())
-        .then((userData) => {
+        .then((res) => {
+          const userData = res.data;
+
           LoginCallBacksuccess(
             {
               id: userData.id,
               nickname: userData.kakao_account.profile.nickname,
             },
-            "naver"
+            "kakao"
           );
-          // 3. 메인 창으로 메시지 보내기
+
+          // 3. 메인 창으로 메시지 보내기 등 추가 작업
         })
-        .catch((err) => console.error("카카오 로그인 에러", err));
+        .catch((err) => {
+          console.error(
+            "카카오 로그인 에러:",
+            err.response?.data || err.message
+          );
+        });
     }
 
     if (state === "naver") {
-      fetch("/oauth2.0/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-          grant_type: "authorization_code",
-          client_id: NAVER_CLIENT_KEY,
-          client_secret: NAVER_SECRET_KEY,
-          redirect_uri: NAVER_REDIRECT_URI,
-          code: code,
-          state: state,
-        }),
-      })
-        .then((res) => res.json())
-        .then((tokenData) =>
-          fetch("/v1/nid/me", {
-            method: "GET",
+      axios
+        .post(
+          "/oauth2.0/token",
+          new URLSearchParams({
+            grant_type: "authorization_code",
+            client_id: NAVER_CLIENT_KEY,
+            client_secret: NAVER_SECRET_KEY,
+            redirect_uri: NAVER_REDIRECT_URI,
+            code: code,
+            state: state,
+          }),
+          {
             headers: {
-              Authorization: `Bearer ${tokenData.access_token}`,
+              "Content-Type": "application/x-www-form-urlencoded",
             },
-          })
+          }
         )
-        .then((res) => res.json())
-        .then((userData) => {
-          console.log("\n\n\n 네이버  \n\n\n", userData);
+        .then((res) => {
+          const accessToken = res.data.access_token;
+
+          return axios.get("/v1/nid/me", {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+        })
+        .then((res) => {
+          const userData = res.data;
+          console.log("\n\n\n 네이버 \n\n\n", userData);
+
           LoginCallBacksuccess(
             {
               id: userData.response.id,
@@ -105,6 +115,12 @@ export default function KakaoCallback() {
               email: userData.response.email,
             },
             "naver"
+          );
+        })
+        .catch((err) => {
+          console.error(
+            "네이버 로그인 실패:",
+            err.response?.data || err.message
           );
         });
     }
